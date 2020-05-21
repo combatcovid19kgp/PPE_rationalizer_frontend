e71a89e3f90aba210576f58ce09d5e6aec219fc3
Eiden0 <shibashis12@gmail.com>
2020-05-21 11:07:21 +0530

    added tabs

--- src/component/demand/demand.jsx
+++ src/component/demand/demand.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import Tabs from 'react-bootstrap/Tabs'
 import './demand.css'
-import '../Css/base.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Tab from "react-bootstrap/Tab";
+import Opd from "./OPD/opd";
+
 
 class Demand extends Component{
     render() {
         return(
-            <div className='title_header'>
-                Demand
+            <div id='main-demand'>
+                <div className='title_header'>
+                    Demand
+                </div>
+                <div className='tabs'>
+                    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                        <Tab eventKey="OA" title="Overall">
+
+                        </Tab>
+                        <Tab eventKey="OPD" title="OPD">
+                            <Opd/>
+                        </Tab>
+                        <Tab eventKey="QT" title="Quarantine">
+
+                        </Tab>
+                        <Tab eventKey="OAC" title="Old Age Center">
+
+                        </Tab>
+                        <Tab eventKey="MS" title="Medical Store">
+
+                        </Tab>
+                    </Tabs>
+                </div>
             </div>
         )
     }
