d24b06906f07c4755d458b782b37e19142a2e9df
Eiden0 <shibashis12@gmail.com>
2020-06-10 02:46:00 +0530

    added date viewer

--- src/component/demand/OPD/opd.jsx
+++ src/component/demand/OPD/opd.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './opd.css'
 import Role from "../Role/role";
-import calendar from '../../assets/images/calendar.png'
 
 class Opd extends Component{
     render() {
         return(
             <React.Fragment>
                 <div className='date-viewer'>
-                    <i className="far fa-calendar fa-lg"/>
-                    <span className='date'>01/01/2000 - 01/01/2000</span>
+                    <span className='date-main'>
+                        <i className="far fa-calendar fa-lg"/>
+                        <span className='date'>01/01/2000 - 01/01/2000</span>
+                    </span>
                 </div>
                 <div className='main1'>
                     <div className='sub_header'>
