4832e10cee372d8eaa8e5446e75bbc35b19321b1
Eiden0 <shibashis12@gmail.com>
2020-07-11 01:28:33 +0530

    deplo error fix

--- src/component/demand/Save/save.jsx
+++ src/component/demand/Save/save.jsx
@@ -3,13 +3,10 @@ import "./save.css";
 import Button from "react-bootstrap/Button";
 
 class Save extends Component {
-	constructor(props) {
-	    super(props);
-	}
 	render() {
 	    return(
 	    		<div className='button-container'>
-							<Button variant='light'> Save </Button>
+							<Button variant='light' > Save </Button>
 	    		</div>
         )
 	}
